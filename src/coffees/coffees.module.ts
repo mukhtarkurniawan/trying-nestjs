bb3bc11b43e4d61f3e3943607e2ae01f5f699415
mukhtarkurniawan <muifakurnaiwan@gmail.com>
2021-06-09 10:04:40 +0700

    Finish section 4

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { COFFEE_BRANDS } from './coffees.constant';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 
+export class MockCoffeesService {}
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor])],
   controllers: [CoffeesController],
-  providers: [CoffeesService],
+  providers: [
+    CoffeesService,
+    { provide: COFFEE_BRANDS, useValue: ['Buddy Brew', 'Nescafe'] },
+  ],
+  exports: [CoffeesService],
 })
 export class CoffeesModule {}
